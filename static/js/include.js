e33f8f4abea54817cf61044fbb93557712a32b44
MehvixDesktop <mehvix@protonmail.com>
2020-07-19 14:10:57 -0500

    cleaned up stuff

---
+++ static/js/include.js
@@ -0,0 +1,10 @@
+void async function (where, html) {
+    //get the imported document in templates:
+    var templates = document.createElement( 'template' )
+    templates.innerHTML = await ( await fetch( 'templates.html' ) ).text()
+
+    var footer = templates.content.querySelector('#footer')
+
+    document.getElementById("footer").insertAdjacentHTML('afterbegin', footer.innerHTML)
+    document.getElementById("copy").insertAdjacentHTML("afterbegin", "© " + new Date().getFullYear() + " - Max Vogel")
+} ()
