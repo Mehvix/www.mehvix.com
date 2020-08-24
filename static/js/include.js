a335a8999458be42b58f2b38c71a1d470a655eeb
MehvixDesktop <mehvix@protonmail.com>
2020-08-24 16:33:15 -0500

    updated todo, messed around w footer, fixed code highlight

--- static/js/include.js
+++ static/js/include.js
@@ -1,10 +1,11 @@
-void async function (where, html) {
-    //get the imported document in templates:
-    var templates = document.createElement( 'template' )
-    templates.innerHTML = await ( await fetch( 'templates.html' ) ).text()
-
-    var footer = templates.content.querySelector('#footer')
-
-    document.getElementById("footer").insertAdjacentHTML('afterbegin', footer.innerHTML)
-    document.getElementById("copy").insertAdjacentHTML("afterbegin", "© " + new Date().getFullYear() + " - Max Vogel")
-} ()
+// void async function (where, html) {
+//     //get the imported document in templates:
+//     var templates = document.createElement( 'template' )
+//     templates.innerHTML = await ( await fetch( 'templates.html' ) ).text()
+//
+//     var footer = templates.content.querySelector('#footer')
+//
+//     document.getElementById("footer").insertAdjacentHTML('afterbegin', footer.innerHTML)
+//     document.getElementById("copy").insertAdjacentHTML("afterbegin", "© " + new Date().getFullYear() + " - Max Vogel")
+// } ()
+// /*todo work on this^*/
