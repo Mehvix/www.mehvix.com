4f352dc5bbfbf7ea8082c2edb701463ba6325439
MehvixDesktop <mehvix@protonmail.com>
2021-04-17 00:02:15 -0500

    rss gets blog contents, new post

--- posts/rssgen.py
+++ posts/rssgen.py
@@ -4,17 +4,22 @@ import os
 import datetime
 import time
 from email import utils
+from xml.sax.saxutils import unescape
 
-# todo append <article> tag contents
+# todos:
+# append <article> tag contents
+# figure out how to do <description>
 
-# idk why this is wack
-fin = open('posts/index.html').read()
+fin = open('posts/index.html', encoding='utf-8').read()
 fout = 'posts/rss.xml'
 soup = BeautifulSoup(fin, 'html.parser')
 
 
 mini = minidom.Document()
 content = mini.createElement('rss')
+content.setAttribute('version', '2.0')
+mini.appendChild(content)
+
 channel = content.appendChild(mini.createElement('channel'))
 
 titleElement = mini.createElement('title')
@@ -51,10 +56,14 @@ channel.appendChild(catElement)
 
 
 for i in soup.find_all("tr"):
-
-    title = i.find("a").string
+    title = i.find("a").string.strip()
     url = i.find("a")['href']
     date = "20" + i.find("td").string
+
+    contPath = 'posts/' + url.split("/")[-1]
+    contFile = open(contPath, encoding='utf-8').read()
+    contSoup = BeautifulSoup(contFile, 'html.parser')
+    cont = str(contSoup.find("article"))
 
     date = utils.formatdate(time.mktime(
         (datetime.datetime.strptime(date, '%Y-%m-%d')).timetuple()))
@@ -86,11 +95,24 @@ for i in soup.find_all("tr"):
     guidElement.appendChild(guidContent)
     itemElement.appendChild(guidElement)
 
+    contElement = mini.createElement('content:encoded')
+    contContent = mini.createTextNode(cont)
+    contElement.appendChild(contContent)
+    itemElement.appendChild(contElement)
+
     channel.appendChild(itemElement)
 
+# with codecs.open(mini.toxml(), "r", "utf-8") as inp:
+#     xml = minidom.parseString(inp.read().encode("utf-8"))
+# with codecs.open(fout, "w", "utf-8") as out:
+#     xml.writexml(out)
 
-content = content.toprettyxml(indent="\t")
+# with codecs.open(fout, "w", "utf-8") as out:
+#     mini.writexml(out)
 
+# f = open(fout, 'w', encoding='utf-8')
+# mini.writexml(f, indent="\t", newl="\n", encoding='utf-8')
+# f.close()
 
-with open(fout, "w") as f:
-    f.write(content)
+with open(fout, "w", encoding='utf-8') as f:
+    f.write(unescape(mini.toprettyxml(indent="\t", newl="\n")))
